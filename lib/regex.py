da52362af989ae5aea61067cc07f66a66b01f6c5
michellemwangi01 <michellemwangim@gmail.com>
2023-08-29 08:48:41 +0300

    completed

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,17 @@
 import re
 
-my_pattern = r""
+FINDALL_STRING = """
+    It's such a lovely day today.
+    Where'd all the time go?
+    Some weather we're having today, huh?
+    Tomorrow never knows!
+    Maybe today's just not my day.
+    It's clobbering time!
+"""
+
+my_pattern = r"It's such a lovely day today\.|Maybe today's just not my day\.|Some weather we're having today, huh\?"
 my_regex = re.compile(my_pattern)
+matches = re.findall(my_pattern, FINDALL_STRING)
+
+
 
